d08988f6b387724e1b1dbcd1af6713d7ad8ed2e5
Julho Michalak <julhomichalak@gmail.com>
2023-07-01 09:51:13 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+// Exercício 1
+alert("Hello World!");
+
+// Exercício 2
+var a = 5;
+var b = 3;
+var soma = a + b;
+console.log("A soma é: " + soma);
+
+// Exercício 3
+var numero = 10;
+if (typeof numero === "number") {
+  console.log("É um número");
+} else {
+  console.log("Não é um número");
+}
+
+// Exercício 4
+var texto = "Hello";
+if (typeof texto === "string") {
+  console.log("É uma string");
+} else {
+  console.log("Não é uma string");
+}
+
+// Exercício 5
+var booleano = true;
+if (typeof booleano === "boolean") {
+  console.log("É um booleano");
+} else {
+  console.log("Não é um booleano");
+}
+
+// Exercício 6
+var x = 8;
+var y = 3;
+var subtracao = x - y;
+console.log("A subtração é: " + subtracao);
+
+// Exercício 7
+var c = 4;
+var d = 5;
+var multiplicacao = c * d;
+console.log("A multiplicação é: " + multiplicacao);
+
+// Exercício 8
+var e = 10;
+var f = 2;
+var divisao = e / f;
+console.log("A divisão é: " + divisao);
+
+// Exercício 9
+var numPar = 6;
+if (numPar % 2 === 0) {
+  console.log("É um número par");
+} else {
+  console.log("Não é um número par");
+}
+
+// Exercício 10
+var numImpar = 7;
+if (numImpar % 2 !== 0) {
+  console.log("É um número ímpar");
+} else {
+  console.log("Não é um número ímpar");
+}
